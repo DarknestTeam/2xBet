8d414f2799718d46c46b1d5a13aebcef4df70337
Артемон <@DESKTOP-UT3V00N>
2018-09-10 16:19:32 +0300

    v 0.1

---
+++ 2xBet.DAL/Entities/Bet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.DAL.Entities
+{
+    class Bet
+    {
+        public int Bet_Id { get; set; }
+        public int MatchId { get; set; }
+        public int UserId { get; set; }
+        public int Amount { get; set; }
+        public string Choise { get; set; }
+        public DateTime Date { get; set; }
+        public string Result { get; set; }
+    }
+}
---
+++ 2xBet.DAL/Entities/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.DAL.Entities
+{
+    class Card
+    {
+        public int CardId { get; set; }
+        [Required]
+        [DataType(DataType.CreditCard)]
+        public string NumberCard { get; set; }
+        [Required]
+        [MaxLength(5, ErrorMessage = "формат должен быть : 01/21")]
+        public string Term { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "не более 20 букв")]
+        public string CardHolderName { get; set; }
+        [Required]
+        [MaxLength(3, ErrorMessage = "не более 3 символов кода")]
+        public string Code { get; set; }
+    }
+}
