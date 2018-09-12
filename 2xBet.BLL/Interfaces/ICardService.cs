f0ccebde35bed7b691cdabd85edf777f84363ece
Артемон <@DESKTOP-UT3V00N>
2018-09-13 00:46:04 +0300

    fix2

---
+++ 2xBet.BLL/Interfaces/ICardService.cs
@@ -0,0 +1,14 @@
+﻿using _2xBet.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.BLL.Interfaces
+{
+   public interface ICardService
+    {
+        void AddCard(CardDTO card);
+    }
+}
