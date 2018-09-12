ce890f81480b9a9df7c3bea0d15204dfeb7ccb9a
Артемон <@DESKTOP-UT3V00N>
2018-09-13 00:38:00 +0300

    added service

---
+++ 2xBet.BLL/Services/Service.cs
@@ -0,0 +1,37 @@
+﻿using _2xBet.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.BLL.Services
+{
+    class Service : IService<T>
+    {
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
