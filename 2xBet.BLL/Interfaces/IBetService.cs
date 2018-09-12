9a6aacd0d418df12b032361ada8fd68c10b9acf8
андрейка\андрейКов <romann677@gmail.com>
2018-09-13 02:06:50 +0300

    maybe 2.0)

---
+++ 2xBet.BLL/Interfaces/IBetService.cs
@@ -0,0 +1,18 @@
+﻿using _2xBet.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.BLL.Interfaces
+{
+    public  interface IBetService
+    {
+        void CraeateBet(BetDTO betDTO);
+        void Delete(int Id);
+        BetDTO Get(int id);
+        IEnumerable<BetDTO> Bets();
+        void ChangeBet(BetDTO betDTO);
+    }
+}
