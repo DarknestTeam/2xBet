697192bb4554d1ff7b249c444bd385e7455ced49
андрейка\андрейКов <romann677@gmail.com>
2018-09-13 02:02:29 +0300

    maybe

---
+++ 2xBet.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using _2xBet.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2xBet.BLL.Interfaces
+{
+     public interface IUserService
+    {
+        void MakeUser(UserDTO userDTO);
+        UserDTO Get(int? id);
+        void UpdateUser(UserDTO user);
+        void Delete(int? id);
+        void DepositAccount(UserDTO userDTO, int id);
+        void AddCard(CardDTO card);
+    }
+}
